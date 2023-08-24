f95ce1d40aeeba8d31d48ff29a5757baefa79a09
Subhadip Chakraborty <subhadipc992@gmail.com>
2023-08-24 21:44:13 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,6 @@
+const btn = document.getElementById("btn");
+let para = document.getElementById("text");
+
+btn.addEventListener("click", function () {
+  para.innerHTML = "welcome to the world of Javascript!";
+});
